844ecf9bd8fc23dd2ad3db3c5f12adea658e1d60
Mohammad Aburihan <mohammadre1654@gmail.com>
2025-06-11 23:07:11 +0300

    remove delay from MEMS.h

--- CAR/Motor/mems.h
+++ CAR/Motor/mems.h
@@ -4,6 +4,22 @@
 #define SECONDMDMEMSIN1 49
 #define SECONDMDMEMSIN2 47
 
+// Variables For Timing MEMS1
+unsigned long startMillisMEMS1;  //some global variables available anywhere in the program
+unsigned long currentMillisMEMS1;
+const unsigned long periodMEMS1 = 50;  //the value is a number of milliseconds
+
+int valueMEMS1 = HIGH;
+int valueSecondMEMS1 = LOW;
+
+// Variables For Timing MEMS2
+unsigned long startMillisMEMS2;  //some global variables available anywhere in the program
+unsigned long currentMillisMEMS2;
+const unsigned long periodMEMS2 = 50;  //the value is a number of milliseconds
+
+int valueMEMS2 = HIGH;
+int valueSecondMEMS2 = LOW;
+
 void SetupMems() {
   pinMode(FIRSTMDMEMSIN1, OUTPUT);
   pinMode(FIRSTMDMEMSIN2, OUTPUT);
@@ -14,20 +30,36 @@ void SetupMems() {
 
 void FirstMems() {
   // analogWrite(MDMEMSENB, 255);
-  digitalWrite(FIRSTMDMEMSIN1,LOW);
-  digitalWrite(FIRSTMDMEMSIN2,HIGH);
-  delay(50);
-  digitalWrite(FIRSTMDMEMSIN1,HIGH);
-  digitalWrite(FIRSTMDMEMSIN2,LOW);
-  delay(50);
+
+  currentMillisMEMS1 = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+  if (currentMillisMEMS1 - startMillisMEMS1 >= periodMEMS1)  //test whether the period has elapsed
+  {
+    // TODO
+    digitalWrite(FIRSTMDMEMSIN1,valueMEMS1);
+    digitalWrite(FIRSTMDMEMSIN2,valueSecondMEMS1);
+    valueMEMS1 = (valueMEMS1 == HIGH) ? LOW : HIGH;
+    valueSecondMEMS1 = (valueSecondMEMS1 == HIGH) ? LOW : HIGH;
+    startMillisMEMS1 = currentMillisMEMS1;  //IMPORTANT to save the start time of the current LED state.
+  }
+
+  // digitalWrite(FIRSTMDMEMSIN1,LOW);
+  // digitalWrite(FIRSTMDMEMSIN2,HIGH);
+  // delay(50);
+  // digitalWrite(FIRSTMDMEMSIN1,HIGH);
+  // digitalWrite(FIRSTMDMEMSIN2,LOW);
+  // delay(50);
 }
 
 void SecondMems() {
   // analogWrite(MDMEMSENB, 255);
-  digitalWrite(SECONDMDMEMSIN1,LOW);
-  digitalWrite(SECONDMDMEMSIN2,HIGH);
-  delay(50);
-  digitalWrite(SECONDMDMEMSIN1,HIGH);
-  digitalWrite(SECONDMDMEMSIN2,LOW);
-  delay(50);
+  currentMillisMEMS2 = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+  if (currentMillisMEMS2 - startMillisMEMS2 >= periodMEMS2)  //test whether the period has elapsed
+  {
+    // TODO
+    digitalWrite(SECONDMDMEMSIN1,valueMEMS2);
+    digitalWrite(SECONDMDMEMSIN2,valueSecondMEMS2);
+    valueMEMS2 = (valueMEMS2 == HIGH) ? LOW : HIGH;
+    valueSecondMEMS2 = (valueSecondMEMS2 == HIGH) ? LOW : HIGH;
+    startMillisMEMS2 = currentMillisMEMS2;  //IMPORTANT to save the start time of the current LED state.
+  }
 }
